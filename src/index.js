47d0c627b45d46578859755d0028b4d5c12fd5f1
Ahmed-Elmmohamedy <112728831+Ahmed-Elmmohamedy@users.noreply.github.com>
2026-05-22 20:39:55 +0300

    Add governorates route to the application

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const uploadRoutes = require("./routes/upload");
 const categoryRoutes = require("./routes/categories");
 const couponRoutes = require("./routes/coupons");
 const blacklistRouter = require("./routes/blacklist");
+const governoratesRouter = require("./routes/governorates");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,6 +27,7 @@ app.use("/api/upload", uploadRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/blacklist", blacklistRouter);
+app.use("/api/governorates", governoratesRouter);
 
 app.use((req, res) => res.status(404).json({ error: "Route not found" }));
 app.use((err, req, res, next) => {
